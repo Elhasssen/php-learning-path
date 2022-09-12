806be13c098b3608cd698e759875fad0c2358e82
ElHassen <Boumeddiene.elhassen@gmail.com>
2022-09-12 03:28:04 +0100

    output

---
+++ 01_output.php
@@ -0,0 +1,33 @@
+<?php
+    // echo - output string numbers, html, class
+    // echo 123, 'Hello', 10.5;
+
+    // print - works lik echo, but take only one argument
+    // print 123
+
+    // print_r() print sinlg e values and arrays
+    // print_r([1,2,3])
+
+    // var_dumb() returns mpre infos about that varibals
+    // var_dump(123)
+
+    // var_export( - similar to var_dump()) output a string
+    // representation of a variabale
+
+    // var_export(123);
+?>
+
+<!-- // in case of fetching from a database -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> <?php  echo 'varibale one '?></h1>
+</body>
+</html>
